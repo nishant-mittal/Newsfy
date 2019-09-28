87b04a39cabb0785a789d2f453a522425a7b4e35
nishant <nishantmittal80@gmail.com>
2019-09-28 12:13:22 +0530

    Added share button

--- app/src/main/java/com/example/android/newsfy/NewsActivity.java
+++ app/src/main/java/com/example/android/newsfy/NewsActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.newsfy;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,7 +24,7 @@ public class NewsActivity extends AppCompatActivity {
         TextView newsDate;
         TextView newsContent;
         TextView newsWebLink;
-
+        FloatingActionButton floatingActionButton;
         final int position = getIntent().getExtras().getInt("position");
 
         newsImageView = findViewById(R.id.news_view_image);
@@ -32,6 +33,7 @@ public class NewsActivity extends AppCompatActivity {
         newsDate = findViewById(R.id.news_view_date);
         newsContent = findViewById(R.id.news_view_content);
         newsWebLink = findViewById(R.id.news_view_website_link);
+        floatingActionButton = findViewById(R.id.floating_action_button);
 
         Picasso.get().load(MainActivity.mNews.get(position).getNewsImageURL()).fit().centerInside().into(newsImageView);
         newsTitle.setText(MainActivity.mNews.get(position).getNewsTitle());
@@ -45,6 +47,22 @@ public class NewsActivity extends AppCompatActivity {
                 Intent intent = new Intent(NewsActivity.this, NewsDisplayActivity.class);
                 intent.putExtra("position", position);
                 startActivity(intent);
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.check_this_out);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, MainActivity.mNews.get(position).getNewsUrl());
+                sendIntent.setType("text/plain");
+                String shareText = getString(R.string.share_via);
+                Intent chooser = Intent.createChooser(sendIntent, shareText);
+                if (sendIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(chooser);
+                }
             }
         });
 
